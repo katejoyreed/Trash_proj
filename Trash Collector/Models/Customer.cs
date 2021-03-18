9612e570e83fa7eff798a4ec4d0592b5dd5cd567
katejoyreed <katejoyreed@gmail.com>
2021-03-18 17:34:14 -0500

    Added Identity-related FKs to Customer Model

---
+++ Trash Collector/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID;
+
+        [Display(Name = "First Name")]
+        public string FirstName;
+        [Display(Name = "Last Name")]
+        public string LastName;
+        public string Address;
+        [Display(Name = "Trash Day")]
+        public DayOfWeek TrashDay;
+        [Display(Name = "Balance Due")]
+        public int Balance;
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+    }
+}
