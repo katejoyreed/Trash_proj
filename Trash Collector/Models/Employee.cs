d311b8bff785695c3bec55f38f10039a65ece797
katejoyreed <katejoyreed@gmail.com>
2021-03-18 17:58:28 -0500

    Added FKs to Employee Model

---
+++ Trash Collector/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID;
+
+        [Display(Name = "First Name")]
+        public string FirstName;
+        [Display(Name = "Last Name")]
+        public string LastName;
+        [Display(Name = "Zip Code")]
+        public int ZipCode;
+        
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
